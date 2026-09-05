a3e09c0ec5b5e20b7ae6312c991f86725b9da0da
Obabueki Ewain <ewainosasere.obabueki@univaciti.com>
2026-09-05 18:45:56 +0100

    Fix Sequelize connection to use DATABASE_URL and dialectModule for Vercel

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
+const pg = require('pg');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  { host: process.env.DB_HOST, port: process.env.DB_PORT, dialect: 'postgres', logging: false }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
+  logging: false,
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false }
+  }
+});
 
 const Customer = require('./customer')(sequelize, DataTypes);
 const Restaurant = require('./restaurant')(sequelize, DataTypes);
