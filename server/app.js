8ed790743c57e118048e5b9071f6b2cfab9a62d4
Obabueki Ewain <ewainosasere.obabueki@univaciti.com>
2026-09-05 13:12:03 +0100

    Restructure backend for Vercel serverless deployment

---
+++ server/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const menuRoutes = require('./routes/menu');
+const staffRoutes = require('./routes/staff');
+const orderRoutes = require('./routes/orders');
+const complaintRoutes = require('./routes/complaints');
+const paymentRoutes = require('./routes/payments');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/menu', menuRoutes);
+app.use('/api/staff', staffRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/complaints', complaintRoutes);
+app.use('/api/payments', paymentRoutes);
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,23 +1,5 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const app = require('./app');
 const { sequelize } = require('./models');
-
-const menuRoutes = require('./routes/menu');
-const staffRoutes = require('./routes/staff');
-const orderRoutes = require('./routes/orders');
-const complaintRoutes = require('./routes/complaints');
-const paymentRoutes = require('./routes/payments');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/menu', menuRoutes);
-app.use('/api/staff', staffRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/complaints', complaintRoutes);
-app.use('/api/payments', paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
 
